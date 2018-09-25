69636db38c2d0fe86c5f903473e129399520c539
pleyus <pleyus@gmail.com>
2018-09-25 17:40:26 -0500

    El log se pasó a github, los cumpleaños ahora muestran imagen de usuario.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,40 +11,23 @@ import { Router } from '../../../node_modules/@angular/router';
 })
 export class HomeComponent {
 
-	constructor( 
-		public $ : AppComponent, 
-		public T:Tools,
-		private C: Configuration,
-		public S: AppStatus,
-		private R: Router
-	){
-		if( $.isAdmin() ) 
-		{
-			$.ngOnInit();
-			this.Oktext();
-		}
-		else
-			R.navigate(['/me']);
-	}
-  	public GetOption(option, context = 'main', def = false){
-		return this.C.GetOption(option, context);
-	}
-	public SetOption(option, value, context = 'main'){
-		this.C.SetOption(option, value, context);
-	}
-	oktextos = [
-		'No me interesa',
-		'Me da igual',
-		'Aceptar',
-		'Aceptar',
-		'Ya estas...',
-		'Vale',
-	];
-	ok = '';
-	Oktext()
-	{
-		let i = this.T.Random(0, this.oktextos.length);
-		this.ok = this.oktextos[i];
-	}
-
+  constructor(
+    public $: AppComponent,
+    public T: Tools,
+    private C: Configuration,
+    public S: AppStatus,
+    private R: Router
+  ) {
+    if ( $.isAdmin() ) {
+      $.ngOnInit();
+    } else {
+      R.navigate(['/me']);
+    }
+  }
+  public GetOption(option, context = 'main', def = false) {
+    return this.C.GetOption(option, context);
+  }
+  public SetOption(option, value, context = 'main') {
+    this.C.SetOption(option, value, context);
+  }
 }
