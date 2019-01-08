79f87a2aed8041bea4a63878281d3824c955c043
pleyus <pleyus@gmail.com>
2019-01-08 13:30:53 -0600

    Movido el menu principal
    Se movió el menú principal a la parte superior. Ahora es responsivo.
    
    Tambien se reparó el wrap.

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { AppComponent } from '../../app.component';
 import { DOCUMENT } from '@angular/common';
-import { Tools } from '../../app.service';
+import { Tools, AppStatus } from '../../app.service';
 
 @Component({
   selector: 'app-core-header',
@@ -9,8 +9,15 @@ import { Tools } from '../../app.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  public MENU_RESPONSIVE = false;
+
   constructor(
     public $: AppComponent,
     public T: Tools,
+    public S: AppStatus,
     @Inject(DOCUMENT) document) {}
+
+  public MenuClick(Set: boolean = null) {
+    this.MENU_RESPONSIVE = Set === null ? !this.MENU_RESPONSIVE : Set;
+  }
 }
