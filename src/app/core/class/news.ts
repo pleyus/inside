99e2766ed8d4ee56c09b58cbbb07cf0dbf01d838
pleyus <pleyus@gmail.com>
2019-02-23 10:09:32 -0600

    Interfaz de las noticias (counters)

---
+++ src/app/core/class/news.ts
@@ -0,0 +1,9 @@
+export class NewsAdapter {
+  constructor(
+    public Applicants: number = 0,
+    public RadioMessages: number = 0,
+    public Messages: number = 0,
+    public Birthdays: Array<any> = [],
+    public Feedbacks: number = 0
+  ) {}
+}
